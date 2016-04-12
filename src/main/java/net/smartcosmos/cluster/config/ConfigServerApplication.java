8d85e1487509599987949c487bf7575f4b86dd37
Robert Van Voorhees <rcvanvo@gmail.com>
2016-04-12 12:06:48 -0400

    Initial commit to config server's own repository.

---
+++ src/main/java/net/smartcosmos/cluster/config/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package net.smartcosmos.cluster.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+	}
+
+}
